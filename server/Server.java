b08ddbc11a9c0fe75397c2f5765d153cd54b9594
William Pearce <billpearce25@gmail.com>
2023-02-16 16:50:26 -0600

    update 1

--- server/Server.java
+++ server/Server.java
@@ -1,11 +1,19 @@
 import java.net.*;
 import java.io.*;
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.*;
+
 
 public class Server extends Thread {
 
     private static final int PORT = 4545;
 
     public static void main(String[] args) throws Exception {
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("DH");
+        kpg.initialize(Skip.sDHParameterSpec);
+        KeyPair kp = kpg.genKeyPair();
 
         ServerSocket server = new ServerSocket(PORT);
 
--- server/ServerThread.java
+++ server/ServerThread.java
@@ -3,18 +3,28 @@ import java.io.*;
 
 public class ServerThread extends Thread {
 
-    private Socket server;
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
 
-    public ServerThread(Socket sock) {
-        server = sock;
+    public ServerThread(Socket sock) throws Exception {
+        client = sock;
+        BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
     }
     public void run() {
         try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+            // Put what server does here
+            while(true) {
+                echo(in.readLine());
+            }
 
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public void echo(String msg) {
+        out.println(msg);
     }
 }
